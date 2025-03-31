7ad2f9a2c944e0da4d838cbb53e2805bec87acf1
saul-l <sauli.lehtinen@gmail.com>
2025-03-31 17:59:24 +0300

    manual culling with rocket values. buggy anagrams

--- Demo.js
+++ Demo.js
@@ -212,20 +212,20 @@ Demo.prototype.init = function () {
     {start: 9*window.pattern, duration: 48*window.pattern, name: 'investigationBoard', dof:true, polaroid:false},
     {start: 9*window.pattern, duration: 48*window.pattern, name: 'shadow', dof:false, polaroid:false},
     //polaroid scenes
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'chemTrail', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'farjan', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'flatTorus', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'greetings', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'aliens', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'introPolaroid', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'endZoomer', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'AsmA', dof:false, polaroid:true},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'chemTrail', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:chemTrail')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'farjan', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:farjan')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'flatTorus', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:flatTorus')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'greetings', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:greetings')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'aliens', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:aliens')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'introPolaroid', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:introPolaroid')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'endZoomer', dof:false, polaroid:true, parameters:{time:()=>getSceneTimeFromStart(), pause:()=>Sync.get('Pause:endZoomer')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'AsmA', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:AsmA')}}, 
     {start: 9*window.pattern, duration: 48*window.pattern, name: 'chess', dof:false, polaroid:true, parameters:{time:()=>Math.sin(getSceneTimeFromStart()*10)*100, pause:()=>Math.sin(getSceneTimeFromStart()*4)<0.0}},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'anagram', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'mrna', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'deepState', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'plasma', dof:false, polaroid:true},
-    {start: 9*window.pattern, duration: 48*window.pattern, name: 'wordCloud', dof:false, polaroid:true}
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'anagram', dof:false, polaroid:true, parameters:{time:()=>getSceneTimeFromStart(),pause:()=>Sync.get('Pause:anagram')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'mrna', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:mrna')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'deepState', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:deepstate')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'plasma', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:plasma')}},
+    {start: 9*window.pattern, duration: 48*window.pattern, name: 'wordCloud', dof:false, polaroid:true, parameters:{pause:()=>Sync.get('Pause:wordCloud')}},
   ];
 
   scenes.forEach((scene) => {
