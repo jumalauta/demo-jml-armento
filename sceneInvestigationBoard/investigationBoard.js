e6feac8eff55c5959a4b6d855b457787d906b6d2
saul-l <sauli.lehtinen@gmail.com>
2025-04-04 21:49:24 +0300

    added some visual sync stuff. end scene animated papers initial values

--- sceneInvestigationBoard/investigationBoard.js
+++ sceneInvestigationBoard/investigationBoard.js
@@ -23,21 +23,21 @@ Demo.prototype.sceneInvestigationBoard = function () {
 
   // center 
   this.photo15x10(0.0 ,0, 0, -3,.5,'endZoomer.color.fbo');
-  this.textPaperAnimated(124,.1, -.2, 0.3, 5, 0, 2, .35, .2,.12,'???',{visible:false});
-  this.textPaperAnimated(134 ,.1, -.2, 0.3, 5, 0.01, 2, .35, .6 ,.12,'Assembly Summer 2025',{visible:false});
-  this.textPaperAnimated(129,.1, .45, 0.05, 5, 0, 2, .35, .25 ,.12,'290825?',{visible:false});
-  this.textPaperAnimated(133,.1, .51, 0.05, 5, 0.01, 2, .35, .42 ,.12,'31.07.-03.08.2025',{visible:false});
+  this.textPaperAnimated(124,.5, -.2, 0.3, .5, 0, 2, .35, .2,.12,'???',{visible:false});
+  this.textPaperAnimated(134 ,.5, -.2, 0.3, .5, 0.01, 2, .35, .6 ,.12,'Assembly Summer 2025',{visible:false});
+  this.textPaperAnimated(129,.5, .45, 0.05, .5, 0, 2, .35, .25 ,.12,'290825?',{visible:false});
+  this.textPaperAnimated(133,.5, .51, 0.05, .5, 0.01, 2, .35, .42 ,.12,'31.07.-03.08.2025',{visible:false});
 
-  this.textPaperAnimated(137,.1, .51, -0.04, 5, 0.0, 2, .35, .42 ,.12,'MESSUKESKUS',{visible:false});
+  this.textPaperAnimated(137,.5, .51, -0.04, .5, 0.0, 2, .35, .42 ,.12,'MESSUKESKUS',{visible:false});
   
-  //this.textPaperAnimated(125.5,.1, .51, -0.04, 5, 0.0, 2, .35, .42 ,.12,'NIBIRU',{visible:false});
-  this.textPaperAnimated(130,.1, .51, -0.11, 5, 0.01, 2, .35, .40 ,.11,'HÄMEENLINNA',{visible:false});
-  this.textPaperAnimated(132,.1, .505, -0.11, 5, 0.02, -1, .35, .32 ,.11,'HELSINKI',{visible:false});
 
-  this.textPaperAnimated(120.5,.1, .51, -0.19, 5, 0.00, 2, .35, .42 ,.12,'NIBIRU',{visible:false});
-  this.textPaperAnimated(122,.1, .51, -0.19, 5, 0.01, 2, .35, .42 ,.12,'EARTH',{visible:false});
-  this.textPaperAnimated(125,.1, .51, -0.19, 5, 0.02, 2, .35, .42 ,.12,'EUROPE',{visible:false});
-  this.textPaperAnimated(127,.1, .51, -0.19, 5, 0.03, 2, .35, .42 ,.12,'FINLAND',{visible:false});
+  this.textPaperAnimated(130,.5, .51, -0.11, .5, 0.01, 2, .35, .40 ,.11,'HÄMEENLINNA',{visible:false});
+  this.textPaperAnimated(132,.5, .505, -0.11, .5, 0.02, -1, .35, .32 ,.11,'HELSINKI',{visible:false});
+
+  this.textPaperAnimated(120.5,.5, .51, -0.19, .5, 0.00, 6, .35, .26 ,.12,'NIBIRU',{visible:false});
+  this.textPaperAnimated(122,.5, .51, -0.19, .5, 0.01, -2, .35, .27 ,.12,'EARTH',{visible:false});
+  this.textPaperAnimated(125,.5, .51, -0.19, .5, 0.02, 3, .35, .27 ,.12,'EUROPE',{visible:false});
+  this.textPaperAnimated(127,.5, .51, -0.19, .5, 0.03, 1, .35, .3 ,.12,'FINLAND',{visible:false});
   // left bottom corner 
   this.textPaper(0, -1.15, -.71, 6, .35, .5,.5,'',{visible:false});
   this.polaroid(0.0 ,-1.5, -.78, -15.5,.43,'mrna.color.fbo');
@@ -108,7 +108,8 @@ Demo.prototype.sceneInvestigationBoard = function () {
   // bottom center
     // linedraw 1, photo 3
   this.photo15x10(0.0 ,.04, -.88, -5,.35,'bzm.color.fbo',{visible:false});
-
+  this.text(0, .04, -.73, 5, .25,'THE PARTY IS OUT THERE','monoSpace',0);
+  this.textPaper(0,.14, -1.05, 11 , .3, .71,.1,'SUB ROSA SHADOW EVENT',{visible:false});
     // cam 2
   this.polaroid(0.0 ,-.8, .95 , 5,.23,'anagram0.color.fbo',{visible:false});
   this.polaroid(0.0 ,-.65, .375 , 5,.23,'anagram1.color.fbo',{visible:false});
@@ -224,7 +225,8 @@ Demo.prototype.textPaperAnimated = function (startTime, animDur, x, y, zStart, z
     },
     perspective:"3d", 
     color:[{"r":0.0,"g":0.0,"b":0.0}],
-    position:[{x:x, y:y, z:z}],
+    position:[{x:x, y:y, z:zStart},
+    {duration:animDur, z:z}],
     scale: [{ uniform3d: scale }],
     angle:[{degreesZ:zAngle+Math.random()}]
     }]);
@@ -239,9 +241,9 @@ Demo.prototype.textPaperAnimated = function (startTime, animDur, x, y, zStart, z
         {
           x: x,
           y: y,
-          z: z
-        }
-      ],
+          z: zStart
+        },
+        {duration:animDur, z:z}],
       angle: [
         {
           degreesX: 90,
@@ -254,7 +256,7 @@ Demo.prototype.textPaperAnimated = function (startTime, animDur, x, y, zStart, z
   ]);
 }
 
-Demo.prototype.text = function (startTime, x, y, zAngle, scale, textString, font)
+Demo.prototype.text = function (startTime, x, y, zAngle, scale, textString, font, color = 0.0)
 {
 //"multiSceneEffects/handWriting.ttf"
   this.loader.addAnimation([{
@@ -262,7 +264,7 @@ Demo.prototype.text = function (startTime, x, y, zAngle, scale, textString, font
     text:{string:textString,name:"multiSceneEffects/" + font + ".ttf"
     },
     perspective:"3d", 
-    color:[{"r":0.0,"g":0.0,"b":0.0}],
+    color:[{"r":color,"g":color,"b":color}],
     position:[{x:x, y:y}],
     scale: [{ uniform3d: scale }],
     angle:[{degreesZ:zAngle+Math.random()}]
