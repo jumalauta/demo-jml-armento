8cb6e79fecdfc9ac1b5494f944841edcfca16a79
saul-l <sauli.lehtinen@gmail.com>
2025-04-18 10:33:16 +0300

    subliminal messages added to plasma

--- sceneInvestigationBoard/plasma.js
+++ sceneInvestigationBoard/plasma.js
@@ -1,6 +1,11 @@
 
+var flashTexts = 0;
+var currentFlash = 0; 
+
 Demo.prototype.scenePlasma = function () 
 {
+
+
   this.loader.setScene('plasma');
   
     this.loader.addAnimation({fbo:{name:'plasma',action:'begin',storeDepth:false}});
@@ -32,7 +37,21 @@ Demo.prototype.scenePlasma = function ()
           name:["sceneInvestigationBoard/plasma.fs"]
         }
     });
-    
+
+    this.flashText('OBEY');
+    this.flashText('VOTE');
+    this.flashText('BUY');
+    this.flashText('PARTY');
+    this.flashText('DEMO');
+
+    this.loader.addAnimation({
+      runPreFunction: ()=>
+          {
+             currentFlash=Math.floor(1+Math.random()*flashTexts);
+
+          }
+  })
+
     this.loader.addAnimation([
       {
         image: {
@@ -51,4 +70,33 @@ Demo.prototype.scenePlasma = function ()
     ]);
   this.loader.addAnimation({fbo:{name:'plasma',action:'unbind'}});
 
+}
+
+Demo.prototype.flashText = function (text)
+{
+  let myFlash = flashTexts;
+  this.loader.addAnimation([{
+    text:{string:text,name:"multiSceneEffects/monoSpace.ttf"
+    },
+    perspective:'3d', 
+    color:[{"r":()=>Math.random(),"g":()=>Math.random(),"b":()=>Math.random()}],
+    position:[{x:0, y:.2}],
+    scale: [{ uniform3d: ()=>
+      {
+        if(currentFlash == myFlash)
+        {
+          return Math.random()*2.0+3;
+        }
+        else
+        {
+          return 0.0;
+        }
+      }
+       }],
+    material:{
+    // depthTest: !depthSort,
+    //  transparent:depthSort,
+    }
+  }]);
+  flashTexts++;
 }
